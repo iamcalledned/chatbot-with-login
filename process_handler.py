7505c2209bf5830e27a956cdeb7e7771d09664a2
Ned <iamcalledned@gmail.com>
2023-12-12 17:36:51 -0500

    Update process_handler.py

--- process_handler.py
+++ process_handler.py
@@ -79,7 +79,7 @@ async def callback(code: str, state: str):
     tokens = await exchange_code_for_token(code, code_verifier)
     if tokens:
         # Process tokens (e.g., store them in session or database)
-        window.location.href = '/chat.html'
+       return RedirectResponse("/chat.html")
     else:
         raise HTTPException(status_code=400, detail="Error exchanging code for tokens")
 
