bf55d56e347ae219f9289b917a960f381f6924e9
Ned <iamcalledned@gmail.com>
2023-12-15 21:14:54 -0500

    Update classify_content.py

--- classify_content.py
+++ classify_content.py
@@ -1,16 +1,17 @@
 import re
 
+ingredient_pattern = re.compile(r'- \d+/\d*\s+\w+')
+instruction_pattern = re.compile(r'\b\d+\.\s+(Combine|Stir|Mix|Add|Pour|Simmer|Chop|Bake)')
+
 def classify_content(message_content):
-    # Check for Ingredients and Instructions headers
-    has_headers = "**Ingredients:**" in message_content and "**Instructions:**" in message_content
-    
-    # Regular expression for ingredient list items
-    ingredient_pattern = re.compile(r'-\s+\d+\/?\d*\s+\w+')
-    has_ingredient_list = bool(ingredient_pattern.search(message_content))
-    
-    # Regular expression for step-by-step instructions
-    instruction_pattern = re.compile(r'\b\d+\.\s+(Combine|Stir|Mix|Add|Pour|Simmer|Chop|Bake)')
-    has_instructions = bool(instruction_pattern.search(message_content))
-    
-    if has_headers and has_ingredient_list and has_instructions
-        return "recipe"
+
+  if "**Ingredients:**" not in message_content or "**Instructions:**" not in message_content:
+    return
+
+  if not ingredient_pattern.search(message_content):
+    return  
+
+  if not instruction_pattern.search(message_content):
+    return
+
+  return "recipe"
