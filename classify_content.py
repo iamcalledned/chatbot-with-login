49ba78dba152b37ff800c3fd0c48bf626331bc8c
Ned <iamcalledned@gmail.com>
2023-12-15 22:18:20 -0500

    Update classify_content.py

--- classify_content.py
+++ classify_content.py
@@ -1,17 +1,15 @@
 import re
 
-ingredient_pattern = re.compile(r'- \d+/\d*\s+\w+')
-instruction_pattern = re.compile(r'\b\d+\.\s+(Combine|Stir|Mix|Add|Pour|Simmer|Chop|Bake)')
-
 async def classify_content(message_content):
 
-  if "**Ingredients:**" not in message_content or "**Instructions:**" not in message_content:
-    return
+ # Check for recipe-like headers and list formats
+    ingredients_header = re.search(r'\*\*Ingredients:\*\*', message_content)
+    instructions_header = re.search(r'\*\*Instructions:\*\*', message_content)
+    ingredients_list = re.findall(r'-\s+\d+\s+\w+', message_content)
+    instructions_list = re.findall(r'\d+\.\s+[A-Z]', message_content)
 
-  if not ingredient_pattern.search(message_content):
-    return  
-
-  if not instruction_pattern.search(message_content):
-    return
-
-  return "recipe"
+    # Determine if it is a recipe by the presence of headers and lists
+    if ingredients_header and instructions_header and ingredients_list and instructions_list:
+        return "recipe"
+    else:
+        return "other"
