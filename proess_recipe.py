3fd1d8c7b019bfe4d288ff9ed340dfdcaba0e257
Ned <iamcalledned@gmail.com>
2023-12-16 17:46:20 -0500

    Update proess_recipe.py

--- proess_recipe.py
+++ proess_recipe.py
@@ -1,26 +1,9 @@
 import spacy
 from spacy.matcher import Matcher
-from spacy.tokens import Span
-from spacy.language import Language
-
-def set_custom_boundaries(doc):
-    # Custom boundary logic to prevent sentence segmentation at bullet points
-    for token in doc[:-1]:
-        if token.text in ("-"):
-            doc[token.i+1].is_sent_start = False
-    return doc
-
-@Language.component("set_custom_boundaries")
-def set_custom_boundaries_component(doc):
-    return set_custom_boundaries(doc)
-
 
 def parse_recipe_with_spacy(recipe_text):
-    # Load SpaCy model and add custom boundary settings
+    # Load SpaCy model
     nlp = spacy.load("en_core_web_sm")
-    nlp.add_pipe("set_custom_boundaries", before="parser")
-
-    # Process the recipe text
     doc = nlp(recipe_text)
 
     # Variables to hold the sections of the recipe
@@ -29,26 +12,25 @@ def parse_recipe_with_spacy(recipe_text):
     instructions = []
     mode = None
 
-    # Extract title - assuming it's the first noun phrase or sequence of proper nouns
-    for np in doc.noun_chunks:
-        if np.start == 0:
-            title = np.text
+    # Iterate over sentences to find the title
+    for sent in doc.sents:
+        text = sent.text.strip()
+        if '**ingredients**' in text.lower():
             break
-
-    # Process the rest of the text for ingredients and instructions
-    lines = recipe_text.split('\n')
-    for line in lines:
-        line = line.strip()
-
-        if line.lower().startswith('**ingredients:**'):
-            mode = 'ingredients'
-        elif line.lower().startswith('**instructions:**'):
+        if title is None and text and not text.startswith('-') and len(text) < 100:
+            title = text
+            continue
+        if '**instructions**' in text.lower():
             mode = 'instructions'
-        elif line:  # If the line is not empty
-            if mode == 'ingredients':
-                ingredients.append(line)
-            elif mode == 'instructions':
-                instructions.append(line)
+            continue
+        if mode == 'instructions':
+            instructions.append(text)
+        else:
+            ingredients.append(text)
+
+    # Remove empty strings and the title from ingredients and instructions
+    ingredients = [i for i in ingredients if i and i != title]
+    instructions = [i for i in instructions if i and i != title]
 
     return {
         'title': title,
