acb9a58a0c1a8c0e5109f1f8f0d7ce0a6f6e3bd4
Ned <iamcalledned@gmail.com>
2023-12-17 21:12:27 -0500

    Update classify_content.py

--- classify_content.py
+++ classify_content.py
@@ -1,15 +1,13 @@
 import re
 
 async def classify_content(message_content):
+    # Check for the presence of specific phrases
+    recipe_for_header = re.search(r'a recipe for:', message_content, re.IGNORECASE)
+    ingredients_header = re.search(r'Ingredients:', message_content, re.IGNORECASE)
+    directions_header = re.search(r'Directions:', message_content, re.IGNORECASE)
 
- # Check for recipe-like headers and list formats
-    ingredients_header = re.search(r'\*\*Ingredients:\*\*', message_content)
-    instructions_header = re.search(r'\*\*Instructions:\*\*', message_content)
-    ingredients_list = re.findall(r'-\s+\d+\s+\w+', message_content)
-    instructions_list = re.findall(r'\d+\.\s+[A-Z]', message_content)
-
-    # Determine if it is a recipe by the presence of headers and lists
-    if ingredients_header and instructions_header and ingredients_list and instructions_list:
+    # Determine if it is a recipe by the presence of all three headers
+    if recipe_for_header and ingredients_header and directions_header:
         return "recipe"
     else:
         return "other"
