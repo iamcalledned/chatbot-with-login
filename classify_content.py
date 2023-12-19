a18b03228332cdd31cfc56ddfd9723ce0b30a534
Ned <iamcalledned@gmail.com>
2023-12-19 09:37:20 -0500

    Update classify_content.py

--- classify_content.py
+++ classify_content.py
@@ -1,13 +1,20 @@
 import re
 
 async def classify_content(message_content):
-    # Check for the presence of specific phrases
-    recipe_for_header = re.search(r'a recipe for', message_content, re.IGNORECASE)
-    ingredients_header = re.search(r'Ingredients', message_content, re.IGNORECASE)
-    directions_header = re.search(r'Directions', message_content, re.IGNORECASE)
+    # Define patterns that are commonly found in recipes
+    title_pattern = re.compile(r'\b(recipe for|servings|prep time|cook time|total time)\b', re.IGNORECASE)
+    ingredients_pattern = re.compile(r'\b(ingredients|cups?|tablespoons?|teaspoons?|ounces?|pounds?|grams?|kilograms?)\b', re.IGNORECASE)
+    instructions_pattern = re.compile(r'\b(instructions|steps|method|directions)\b', re.IGNORECASE)
+    step_pattern = re.compile(r'\b(step \d+|\d+\.)', re.IGNORECASE)
 
-    # Determine if it is a recipe by the presence of all three headers
-    if recipe_for_header and ingredients_header and directions_header:
+    # Check for patterns in the text
+    has_title = bool(title_pattern.search(message_content))
+    has_ingredients = bool(ingredients_pattern.search(message_content))
+    has_instructions = bool(instructions_pattern.search(message_content))
+    has_steps = bool(step_pattern.search(message_content))
+
+    # Determine if the text is likely a recipe and return the corresponding label
+    if has_title and has_ingredients and has_instructions and has_steps:
         return "recipe"
     else:
         return "other"
