4df88ec7ef537b215f3bb20b74db1e73f1f50156
Ned <iamcalledned@gmail.com>
2023-12-20 14:48:06 -0500

    Update train_ingredient_model.py

--- train_ingredient_model.py
+++ train_ingredient_model.py
@@ -33,37 +33,7 @@ def extract_ingredients():
 
 # Function to process ingredient text and create annotations
 
-def process_ingredient(ingredient):
-    cleaned_ingredient = ingredient.lstrip('- ').strip()
-
-    # Regular expression pattern to match the quantity and unit
-    pattern = r'^([\d\s¼½¾/-]+(?:\([\d\s.]+\))?)([a-zA-Z.]+)?\s*(.*)'
     match = re.match(pattern, cleaned_ingredient)
-    
-    if match:
-        quantity, unit, ingredient_name = match.groups(default='')
-        quantity = quantity.strip()
-        unit = unit.strip() if unit else None
-        ingredient_name = ingredient_name.strip() if ingredient_name else None
-    else:
-        # Fallback for other cases
-        quantity, unit, ingredient_name = None, None, cleaned_ingredient
-
-    # Calculate positions for each entity
-    entities = []
-    if quantity:
-        entities.append((0, len(quantity), "QUANTITY"))
-    if unit:
-        start = len(quantity) + 1
-        entities.append((start, start + len(unit), "UNIT"))
-    if ingredient_name:
-        start_index = len(quantity) + 1  # Start after quantity
-        start_index += len(unit) + 1 if unit else 0  # Add length of unit if present
-        entities.append((start_index, start_index + len(ingredient_name), "INGREDIENT"))
-
-    processed_data = {"text": cleaned_ingredient, "entities": entities}
-    print(processed_data)
-    return processed_data
 
 
 ingredient_texts = extract_ingredients()
