fd0dd6988a77f35d3e29260fbeeb361e055fb162
Ned <iamcalledned@gmail.com>
2023-12-20 15:39:30 -0500

    Update process_recipe.py

--- process_recipe.py
+++ process_recipe.py
@@ -18,11 +18,11 @@ print("loaded nlp from", model_path)
 def analyze_ingredient(ingredient_text):
     # Process the text with spaCy
     cleaned_ingredient_text = ingredient_text.lstrip('- ').strip()
-    print("cleaned ingredient text:", cleaned_ingredient_text)
+    
     pattern = r'\(\d[^)]*\)'
     cleaned_ingredient_text = re.sub(pattern, '', cleaned_ingredient_text)
     cleaned_ingredient_text = re.sub(r'\s+', ' ', cleaned_ingredient_text)  # Collapse multiple spaces into one
-    print("cleaned ingredient text:", cleaned_ingredient_text)
+    
     doc = nlp(cleaned_ingredient_text)
 
     # Extract relevant information (e.g., quantity, unit, ingredient)
